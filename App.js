bff052cba0a9bb88ebfdca85dff42b1fcddcd926
Katie Stairs <kstairs831@gmail.com>
2023-06-12 17:11:42 -0500

    installed prettier and eslint to clean up code

--- App.js
+++ App.js
@@ -1,43 +1,39 @@
-import React from 'react';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>
-            search
-          </Text>
+          <Text>search</Text>
         </View>
         <View style={styles.list}>
-          <Text>
-            list
-          </Text>
+          <Text>list</Text>
         </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight 
+    marginTop: StatusBar.currentHeight,
   },
   text: {
     fontSize: 25,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   search: {
     padding: 16,
-    backgroundColor: 'green'
+    backgroundColor: "green",
   },
-  list : {
+  list: {
     flex: 1,
     padding: 16,
-    backgroundColor: 'blue'
-  }
+    backgroundColor: "blue",
+  },
 });
