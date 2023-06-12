89364596e780bfec5b36a29ec0b5a71c0096ff7e
Katie Stairs <kstairs831@gmail.com>
2023-06-12 12:58:05 -0500

    added andriod statusbar view

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Text>
-          search
-        </Text>
-      </View>
-      <View style={styles.list}>
-        <Text>
-          list
-        </Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>
+            search
+          </Text>
+        </View>
+        <View style={styles.list}>
+          <Text>
+            list
+          </Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight 
   },
   text: {
     fontSize: 25,
     fontWeight: '500',
   },
   search: {
-    padding: 15,
+    padding: 16,
     backgroundColor: 'green'
   },
   list : {
     flex: 1,
-    padding: 15,
+    padding: 16,
     backgroundColor: 'blue'
   }
 });
