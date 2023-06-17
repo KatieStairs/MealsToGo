d41e5218497a63d241febfffe07bcf3ec96deedb
Katie Stairs <kstairs831@gmail.com>
2023-06-17 11:18:47 -0500

    Added tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Text, View } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 // eslint-disable-next-line prettier/prettier
 import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 // eslint-disable-next-line prettier/prettier
 import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => <Text>Settings</Text>;
+const Map = () => <Text>Map</Text>;
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -22,10 +30,40 @@ export default function App() {
     return null;
   }
 
+  // function HomeScreen() {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Home</Text>
+  //     </View>
+  //   );
+  // }
+
+  // function SettingsScreen() {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Settings</Text>
+  //     </View>
+  //   );
+  // }
+
+  // function MapScreen() {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Map</Text>
+  //     </View>
+  //   );
+  // }
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="Map" component={Map} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
